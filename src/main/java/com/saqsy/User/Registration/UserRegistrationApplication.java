aff9205335b52b320e49d6ce05f91a2dcf41ff91
Saquib Kazi <Saquib.kazi99@gmail.com>
2022-07-31 13:38:06 +0530

    Initial Commit

---
+++ src/main/java/com/saqsy/User/Registration/UserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.saqsy.User.Registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationApplication.class, args);
+	}
+
+}
